8f26fdbe813dd6a88ceae501dd3ec0d49b7dab59
Vero Olmos <vero_pampa08@hotmail.com>
2023-10-04 21:37:19 -0300

    Clases 1

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,47 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class ES6 {
+      constructor (usuario,nombre,email,password) {
+         this.usuario= usuario;
+         this.nombre = nombre;
+         this.email= email;
+         this.password = password;
+      }
+
+      saludar () {
+         return ("Hola, mi nombre es "+ this.nombre);
+      };
+   }
+   return (ES6);
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function (){
+      return ("Hello World!")
+   }
+   return (Usuario.saludar);
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function () {
+      var arregloModificado = [];
+
+      for (let i = 0; i< this.length; i++){
+         arregloModificado.unshift(this[i]);
+      }
+      var palabraInversa = "";
+      for (let i= 0; i<this.length; i++) {
+         palabraInversa= palabraInversa+ arregloModificado [i];
+      } return palabraInversa;
+   }
+   return (String.reverse);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
